164666f542dec5a59bdcc268ebb2e8311bc31035
Kamal Huseynaliyev <kamalrh@code.edu.az>
2025-06-10 05:14:50 +0400

    Entities Added

---
+++ Yummy.WebApi/Entities/Feature.cs
@@ -0,0 +1,12 @@
+﻿namespace Yummy.WebApi.Entities
+{
+    public class Feature
+    {
+        public int FutureId { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
